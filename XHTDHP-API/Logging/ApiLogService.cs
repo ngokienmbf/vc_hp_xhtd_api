5fe4d62c64f19be2321b57b5c77f8362533f551a
damvanmanh4495 <manh.damvan10@mobifone.vn>
2022-10-27 10:56:49 +0700

    init project

---
+++ XHTDHP-API/Logging/ApiLogService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace XHTDHP_API.Logging
+{
+    public class ApiLogService
+    { 
+
+        public ApiLogService( )
+        { 
+        }
+    }
+}
