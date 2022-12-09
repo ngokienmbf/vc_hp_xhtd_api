0118288c8ee8e2e488af476e0df579e61bb7eb96
thinhgakon <68736100+thinhgakon@users.noreply.github.com>
2022-12-09 11:27:38 +0700

    Git ignore publish folder & Add scale hub (#40)
    * Git ignore publish folder
    
    * Add scale hub

---
+++ XHTDHP-API/Hubs/HubMethod.cs
@@ -0,0 +1,11 @@
+﻿namespace XHTDHP_API.Hubs
+{
+    static class HubMethod
+    {
+        public const string Send9511ScaleInfo = "Send9511ScaleInfo";
+
+        public const string Send9512ScaleInfo = "Send9512ScaleInfo";
+
+        public const string SendClinkerScaleInfo = "SendClinkerScaleInfo";
+    }
+}
